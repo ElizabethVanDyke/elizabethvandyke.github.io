6ab9e7afc81d180e9892c0b0640131284f9655b8
Elizabeth Van Dyke <elizabeth@dixieranch.com>
2019-06-12 13:29:49 +0000

    Finish gallery

--- js/gallery.js
+++ js/gallery.js
@@ -1,3 +1,31 @@
 function activateGallery() {
-  alert("Hello from the gallery file!");
+    let thumbnails = document.querySelectorAll("#gallery-thumbs > div > img");
+    let mainImage = document.querySelector("#gallery-photo img");
+
+    thumbnails.forEach(function(thumbnail) {
+      // Preload large images.
+    let newImageSrc  = thumbnail.dataset.largeVersion;
+    let largeVersion = new Image();
+    largeVersion.src = thumbnail.dataset.largeVersion;
+    thumbnail.addEventListener("click", function() {
+      // Set clicked image as main image.
+      let newImageSrc = thumbnail.dataset.largeVersion;
+      let imageAlt = thumbnail.dataset.title;
+      mainImage.setAttribute("src", newImageSrc);
+      mainImage.setAttribute("alt", imageAlt);
+      
+      let currentImage = document.querySelector(".current")
+      let currentClass = currentImage.className;
+      currentImage.classList.remove(currentClass);
+      thumbnail.parentNode.classList.add(currentClass);
+      
+       // Update image info.
+      let galleryInfo = document.querySelector("#gallery-info");
+      let title       = galleryInfo.querySelector(".title");
+      let description = galleryInfo.querySelector(".description");
+
+      title.innerHTML       = thumbnail.dataset.title;
+      description.innerHTML = thumbnail.dataset.description;
+    });
+  });
 }
